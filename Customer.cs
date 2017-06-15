2b783bd63c563fd2ee673f02173b13022bc081ee
Wanasak <u510610433@gmail.com>
2017-06-15 23:53:06 +0700

    extract class

--- Customer.cs
+++ Customer.cs
@@ -1,13 +1,30 @@
 namespace refractor4_demo
 {
-    public class Customer
+    public class Address
     {
-        private string _firstName = string.Empty;
-        private string _lastName = string.Empty;
         private string _street = string.Empty;
         private string _city = string.Empty;
         private string _state = string.Empty;
         private int _postalCode = 0;
+
+        public Address(string street, string city, string state, int postalCode)
+        {
+            this._street = street;
+            this._city = city;
+            this._state = state;
+            this._postalCode = postalCode;
+        }
+        
+        public override string ToString()
+        {
+            return _street + " " + _city + " " + _state + " " + _postalCode;
+        }
+    }
+    public class Customer
+    {
+        private string _firstName = string.Empty;
+        private string _lastName = string.Empty;
+        private Address _address = null;
 
         public string GetFirstName()
         {
@@ -22,10 +39,17 @@ namespace refractor4_demo
         {
             this._firstName = firstName;
             this._lastName = lastName;
-            this._street = street;
-            this._city = city;
-            this._state = state;
-            this._postalCode = postalCode;
+            // this._street = street;
+            // this._city = city;
+            // this._state = state;
+            // this._postalCode = postalCode;
+            this._address = new Address(street, city, state, postalCode);
+        }
+        public Customer(string firstName, string lastName, Address address)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._address = address;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace refractor4_demo
         {
             var customer = new Customer("John", "Liquid", "123 Main St", "Chiang Mai", "Muang", 50000);
             System.Console.WriteLine(customer.GetFirstName() + " " + customer.GetLastName());
+
+            var smithAddress =new Address("11/3 Main Road", "London", "Chelsea", 30210);
+            var smith = new Customer("Alan", "Smith", smithAddress);
+            System.Console.WriteLine(smith.GetFirstName() + " " + smith.GetLastName());
+            System.Console.WriteLine(smithAddress.ToString());
         }
     }
 }
